ab5bb19a09ec7ad4eb75dece5cfd876883c7701e
Chubie Mirembe <71077124+ChubieMirembe@users.noreply.github.com>
2020-11-19 13:07:11 +0000

    Add files via upload
    Initial commit, works but has a few missing bit

---
+++ lot_size.py
@@ -0,0 +1,71 @@
+'''
+Simple program to calculate lot size in forex
+'''
+pair_list = ['X', 'GBP', 'USD', 'JPY', 'EUR', 'CAD', 'AUD']
+
+def basec():
+    '''
+    Calculates the lot size if the account currency is the same
+    as the base currency of the pair
+    '''
+    global pip_value
+    price = float(input("Enter the current price of the pair: \n"))
+    pip_value = pip_value*price
+    units = round((pip_value/multiplier), 0)
+    lot_size = round((units/100000), 2)
+
+    print("\nUnits: " + str(units))
+    print("Lot Size: "+ str(lot_size))
+
+def counterc():
+    '''
+    Calculates the lot size if the account currency is the same
+    as the counter currency of the pair
+    '''
+    global pip_value
+    units = round((pip_value/multiplier), 0)
+    lot_size = round((units/100000), 2)
+
+    print("\nUnits: " + str(units))
+    print("Lot Size: "+ str(lot_size))
+
+def neitherc():
+    '''
+    Calculates the lot size if the account currency is not the same
+    as the base/counter currency of the pair
+    '''
+    global pip_value
+    ex_rate = float(input("Enter the exchange rate of your account to the counter currency: \n"))
+    pip_value = pip_value*ex_rate
+    units = round((pip_value/multiplier), 0)
+    lot_size = round((units/100000), 2)
+
+    print("\nUnits: " + str(units))
+    print("Lot Size: "+ str(lot_size))
+
+print('1) GBP \t 4) EUR')
+print('2) USD \t 5) CAD')
+print('3) JPY \t 6) AUD')
+
+account_currency = int(input("Select your account currency \n"))
+account_currency = pair_list[account_currency]
+print(account_currency)
+capital = int(input("Enter you capital: \n"))
+risk = int(input("How much would you like to risk(%): \n"))/100
+stop_loss = int(input("How far away is your stop loss (pips): \n"))
+pair = input("Enter the pair you would like to trade: \n").upper()
+
+if 'JPY' in pair:
+    multiplier = 0.01
+else:
+    multiplier = 0.0001
+
+cash_risk = capital*risk
+pip_value = cash_risk/stop_loss
+
+if str(pair[0:3]) == str(account_currency):
+    basec()
+elif str(pair[3:6]) == str(account_currency):
+    counterc()
+else:
+    neitherc()
